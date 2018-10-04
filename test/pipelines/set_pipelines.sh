4ee6f452ae078e250f60e34689b2595adf5583e8
Dan Isla <disla@google.com>
2018-10-04 09:37:26 -0700

    fix to test task

--- test/pipelines/set_pipelines.sh
+++ test/pipelines/set_pipelines.sh
@@ -3,6 +3,6 @@ READLINK=readlink
 [[ "$(uname)" =~ Darwin ]] && READLINK=greadlink
 BASE_DIR=$($READLINK -f $(dirname $0))
 
-fly -t tf set-pipeline -p terraform-operator -c ${BASE_DIR}/regression-and-prs.yaml -l ${BASE_DIR}/values.yaml
+fly -t tf set-pipeline -p terraform-operator -c ${BASE_DIR}/ci.yaml -l ${BASE_DIR}/values.yaml
 
 fly -t tf expose-pipeline -p terraform-operator
